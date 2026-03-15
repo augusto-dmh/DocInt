7234bcd4bb397cd6cffa9ec252833463a608935f
augusto-dmh <augustodemelohenriques@gmail.com>
2026-03-15 18:34:49 -0300

    fix: use queued broadcast and remove unused SerializesModels

--- app/Events/DocumentStatusUpdated.php
+++ app/Events/DocumentStatusUpdated.php
@@ -6,13 +6,12 @@ use App\Models\Document;
 use Carbon\CarbonImmutable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 
-class DocumentStatusUpdated implements ShouldBroadcastNow
+class DocumentStatusUpdated implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, InteractsWithSockets;
 
     public readonly string $tenantId;
 
